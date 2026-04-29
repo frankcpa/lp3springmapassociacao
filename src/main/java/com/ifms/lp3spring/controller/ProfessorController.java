0dfdae39266b63aac9c2aae95c7f6f43638cd9be
frankcpa <frankcpa84@gmail.com>
2026-04-29 09:48:27 -0400

    salvar professor funcionando

--- src/main/java/com/ifms/lp3spring/controller/ProfessorController.java
+++ src/main/java/com/ifms/lp3spring/controller/ProfessorController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.ifms.lp3spring.Service.ProfessorService;
+import com.ifms.lp3spring.model.ProfessorModel;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfessorController {
@@ -15,6 +17,11 @@ public class ProfessorController {
         return "professor/salvarprofessor";
     }
     
+    @PostMapping("salvarprofessor")
+    public String postSalvar(ProfessorModel professor) {
+        professorService.inserir(professor);
+        return "professor/salvarprofessor";
+    }  
 
     public ProfessorService getProfessorService() {
         return professorService;
